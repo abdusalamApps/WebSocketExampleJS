733babb188fa9ecf3e18a8128b28263ed7f2e125
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2021-03-26 14:34:48 +0100

    function send() onclick

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 var webSocket = new WebSocket("wss://localhost:8080");
 webSocket.send("Here's some text that the server is urgently awaiting!");
+function send() {
+    webSocket.send("Here's some text that the server is urgently awaiting!");
+}
