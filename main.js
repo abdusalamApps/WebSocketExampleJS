a4d447ea5e0d385c16f390c63b8e373cf6191992
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2021-03-26 13:22:02 +0100

    Send message to server

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-val webSocket = new WebSocket("wss://localhost:8080");
+var webSocket = new WebSocket("wss://localhost:8080");
+exampleSocket.send("Here's some text that the server is urgently awaiting!");
