bf4142fc151691b043557030d7d672fdc7c539ef
abdusalamyabrak <abdusalam.yab1996@gmail.com>
2021-03-26 14:10:59 +0100

    Corrected variable name

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 var webSocket = new WebSocket("wss://localhost:8080");
-exampleSocket.send("Here's some text that the server is urgently awaiting!");
+webSocket.send("Here's some text that the server is urgently awaiting!");
